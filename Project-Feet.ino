895b6c07f1a2c5c6e7d16071b2e7ad202b3241c4
Declan <95934184+Dex9999@users.noreply.github.com>
2024-06-03 11:06:51 -0400

    fix data transfer, start wiring/testing sensor

--- Project-Feet.ino
+++ Project-Feet.ino
@@ -1,13 +1,39 @@
 //ultra sonic sensors on front/side to see xy of foot
 //gyro on foot to allow pedal-like movement? (or light sensor)
+#include <Arduino_JSON.h>
+const int trigPin = 9;
+const int echoPin = 10;
+
+long duration;
+int distance;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(57600);
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT); // trigPin
+  pinMode(echoPin, INPUT); // echoPin
 }
 
+const char input[] = "{\"type\":\"mouse\",\"button\":\"left/right/middle/doubleLeft/null\",\"drag\":false,\"newMovement\":{\"x\":5,\"y\":-5},\"scroll\":{\"x\":10,\"y\":15}}";
+JSONVar data;
+JSONVar moveJson;
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("\"mouse\": {\"button\": \"null\",\"drag\": false,\"newMovement\": {\"x\": 5,\"y\": -5},\"scroll\": {\"x\": 10,\"y\": 15}}");
-  delay(2000);
+ 
+
+  // Clear trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets trigPin to HIGH for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // multiply by speed of sound (340m/s 34cm/ms) and divide by two as the sensor
+  // measures the time to travel to object and back
+  distance = duration * 0.034 / 2;
+  data["type"] = "mouse";
+  moveJson["x"] = distance;
+  moveJson["y"] = 0;
+  data["newMovement"] = moveJson;
+  Serial.print("*" + JSON.stringify(data) + "*");
 }
--- index.js
+++ index.js
@@ -4,18 +4,35 @@ let x,y, curr;
 // Create a port
 const port = new SerialPort({
     path: '/dev/ttyACM0',
-    baudRate: 57600, // change later if too slow
+    baudRate: 9600, // change later if too slow
 })
 
-port.on('open', () => {
-    setInterval(() => port.write('active!'), 3000);
-});
+// port.on('open', () => {
+//     setInterval(() => port.write('active!'), 3000);
+// });
 //TODO fix data recieving lmao
+let partial = "";
 port.on('data', (data) => {
-    console.log(`Received serial data: ${data.toString()}`);
-    x = parseInt(data.toString().split(" ")[0]);
-    y = parseInt(data.toString().split(" ")[1]);
-    console.log(x,y)
-    curr = robot.getMousePos();
-    robot.moveMouseSmooth(curr.x+x,curr.y+y)
+    // console.log(`Received serial data: ${data.toString()}`);
+    partial += data.toString();
+    // console.log(partial);
+    const dataParts = partial.split("*");
+    if (dataParts.length > 1) {
+        const fullData = dataParts.shift();
+        partial = dataParts.join("*");
+        if(fullData.startsWith("{") && fullData.endsWith("}")){
+            processData(fullData);
+        } // else ignore, data is corrupted
+    }
+
+    // console.log(x,y)
+    // curr = robot.getMousePos();
+    // robot.moveMouseSmooth(curr.x+x,curr.y+y)
 });
+function processData(data){
+    console.log(data);
+}
+function moveMouse(x, y){
+    let curr = robot.getMousePos();
+    robot.moveMouseSmooth(curr.x+x, curr.y+y);
+}
