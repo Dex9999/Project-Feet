995984e694ec95c2b578bd87bb07717ebc871bd2
Declan <95934184+Dex9999@users.noreply.github.com>
2024-06-18 12:35:50 -0400

    Update Project-Feet.ino

--- Project-Feet.ino
+++ Project-Feet.ino
@@ -71,14 +71,14 @@ void loop() {
 
     if (digitalRead(buttonPin) == LOW) {
       delay(200);
-      currentMode = (currentMode + 1) % 2;  // cycle  modes (0 = servo, 1 = no servo, 2 = pedal)
+      currentMode = (currentMode + 1) % 3;  // cycle  modes (0 = servo, 1 = no servo, 2 = pedal)
     }
 
     if (currentMode == 0) {
       servoTrack();
-    } else if (currentMode == 1)/* {
+    } else if (currentMode == 1) {
       readSensorsOnly();
-    } else if (currentMode == 2) */{
+    } else if (currentMode == 2) {
       pedalMode();
     }
   }
